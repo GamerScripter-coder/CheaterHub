7555e39320b86688716855ee38d25dbcb790715c
GamerScripter-coder <altromatteo7@gmail.com>
2026-06-29 14:01:04 +0200

    Saves

---
+++ Games/84575720768520.lua
@@ -0,0 +1,14 @@
+-- Lucky Block Rush
+
+return function(M, T, IT)
+    local env = getgenv()
+    local successConf, config = pcall(function()
+        return IT.LoadConfig()
+    end)
+
+    local 
+
+    if successConf and config then
+        
+    end
+end
