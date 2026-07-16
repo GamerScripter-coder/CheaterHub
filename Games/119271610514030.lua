6f57c0895479235e3045a45a425d42bd7af4e919
GamerScripter-coder <altromatteo7@gmail.com>
2026-07-16 11:27:13 +0200

    please

--- Games/119271610514030.lua
+++ Games/119271610514030.lua
@@ -349,7 +349,8 @@ end
     local AutoHit = env.AutoHit or false
     local AutoIstantBrainrotPrompt = env.AutoIstantBrainrotPrompt or false
     local AutoFarm = env.AutoFarm or false
-    local Destroying = false
+    local egg
+    local ham
 
     if successConf and config then
         AutoHit = config.AutoHit
@@ -361,7 +362,7 @@ end
         AutoHit = v
 
         while AutoHit do
-            local ham = FindHammer(player.Backpack) or FindHammer(player.Character)
+            ham = FindHammer(player.Backpack) or FindHammer(player.Character)
             ham:Activate()
             task.wait(0.5)
         end
@@ -371,23 +372,14 @@ end
         AutoFarm = v
 
         while AutoFarm do
-            local egg = FindClosestEgg()
+            egg = FindClosestEgg()
             local hum = GetHumanoid()
-            local ham = FindHammer(player.Backpack) or FindHammer(player.Character)
+            ham = FindHammer(player.Backpack) or FindHammer(player.Character)
             hum:EquipTool(ham)
 
             hum:MoveTo(egg.Position)
             hum.MoveToFinished:Wait()
 
-            Destroying = true
-
-            while egg and egg.Parent do
-               if ham then
-                  ham:Activate()
-               end
-
-               task.wait(0.2)
-            end
             task.wait(0.5)
         end
     end)
@@ -439,4 +431,12 @@ end
     AddKeyFunc(Enum.KeyCode.N, function()
         EquipWithFind(velId, "Vel")
     end)
+
+    while egg and egg.Parent do
+        if ham then
+            ham:Activate()
+        end
+
+        task.wait(0.2)
+    end
 end
